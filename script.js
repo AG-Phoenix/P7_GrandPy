f1169b3bde7c3416c5b94ad7f2d135e48450f2ce
AG-Phoenix <github.rnvyu@simplelogin.co>
2022-07-17 23:47:22 +0200

    Initial front end PoC.

---
+++ script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form")
+const div = document.querySelector("#responseBlock")
+const input = document.querySelector("#mainInput")
+
+
+form.addEventListener("submit", askPapy)
+
+function askPapy(event){
+  event.preventDefault()
+  console.log(("askPapy"))
+	fetch("./data.json").then(r => r.json()).then(
+    r => {
+      div.innerHTML += `<p class="question">${input.value}</p>`
+      form.reset()
+      div.innerHTML += `<p class="repsonse">🤖 ${r.response}</p>`
+      input.focus()
+    })
+}
